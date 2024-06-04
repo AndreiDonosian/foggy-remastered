ec2ee1a37dab0b5d12fb94a9855a153826b274c5
Andrew Fillyn <andrewfillyn@gmail.com>
2024-06-04 17:19:17 +0300

    synonym sync

--- resources/views/components/meta.blade.php
+++ resources/views/components/meta.blade.php
@@ -3,5 +3,13 @@
 <meta name="author" content="Avks">
 <title>{{ $title . ' | AvksCloud' }}</title>
 <link rel="icon" href="{{ URL::asset('build/img/logo_avks.svg') }}" sizes="any" type="image/svg+xml">
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-Q4WSDMJHZH"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
+    gtag('config', 'G-Q4WSDMJHZH');
+</script>
 {!! $meta ?? '' !!}
